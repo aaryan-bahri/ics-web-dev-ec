ca67085e0e5b417f95c86664e6d81087ea4cf059
aaryan-bahri <aaryan.bahri_ug2024@ashoka.edu.in>
2025-04-13 16:53:44 +0530

    Day 2 - Code (Initial Comment)

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-button").addEventListener("click", function() {
+    // document.body.style="background-color: beige"
+    alert("Contact Soham @ 9885088080")
+    
+}); 
