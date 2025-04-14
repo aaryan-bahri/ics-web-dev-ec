ebf53aca91f65dfc7b95af9d79616b4503762e97
aaryan-bahri <aaryan.bahri_ug2024@ashoka.edu.in>
2025-04-15 00:27:15 +0530

    Attempt Button Fix

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-document.getElementById("contact-button").addEventListener("click", function() {
+document.getElementById("hide-button").addEventListener("click", function() {
     // document.body.style="background-color: beige"
-    alert("Contact Soham @ 9885088080")
+    // alert("Contact Soham @ 9885088080")
+    document.getElementById("hide-element").style.display="none";
+    
+}); 
+
+document.getElementById("show-button").addEventListener("click", function() {
+    // document.body.style="background-color: beige"
+    // alert("Contact Soham @ 9885088080")
+    document.getElementById("hide-element").style.display="block";
     
 }); 
